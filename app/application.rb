a1d39c9ae7f21de9f5bd47cb839ad4f190e3559b
Ming Duan <11203duan@gmail.com>
2019-05-19 19:05:41 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -18,7 +18,7 @@ class Application
         if @@cart.empty?
           resp.write "Your cart is empty"
         else @@cart.each do |x|
-          resp.write "#{x}"
+          resp.write "#{x}\n"
         end
     else
       resp.write "Path Not Found"
