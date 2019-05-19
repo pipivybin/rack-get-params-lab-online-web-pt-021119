e50ace52c0f55c62952346dfa1e2e7a06e821d34
Ming Duan <11203duan@gmail.com>
2019-05-19 19:12:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -20,6 +20,7 @@ class Application
         else @@cart.each do |x|
           resp.write "#{x}\n"
         end
+      end
     elsif req.path.match(/add/)
       item = get.param[:item]
       if @@item.include?(item)
