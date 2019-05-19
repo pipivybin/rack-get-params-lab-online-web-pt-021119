2ca1083aee59a092e925568692ea415ed9eab7ad
Ming Duan <11203duan@gmail.com>
2019-05-19 19:10:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,7 +26,7 @@ class Application
         @@cart << item
         resp.write "We just added #{item}"
       else resp.write "We don't have that item"
-
+      end
     else
       resp.write "Path Not Found"
     end
